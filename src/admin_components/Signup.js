c784d2526cd7aea42a26e6c1ea213f2ed4c75ae7
Ajayram007 <148475116+Ajayram007@users.noreply.github.com>
2025-11-25 16:01:57 +0530

    Update Signup.js

--- src/admin_components/Signup.js
+++ src/admin_components/Signup.js
@@ -24,7 +24,7 @@ const Signup = () => {
     setMessage("");
 
     try {
-      const res = await axios.post("/signup", formData);
+      const res = await axios.post("https://treasurehuntbackend-y0nc.onrender.com/signup", formData);
       setMessage(res.data.message);
       setFormData({ name: "", password: "", role: "", department: "", phonenumber: "" });
     } catch (error) {
